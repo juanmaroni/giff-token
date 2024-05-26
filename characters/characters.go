3833ea92786453f5ab9f2d88d9f9455c133bfc64
Juanma Romero <pro@juanmaroni.me>
2024-05-26 09:15:08 +0200

    Changing characters functions into constants

--- characters/characters.go
+++ characters/characters.go
@@ -1,5 +1,10 @@
 package characters
 
+const ALPHABETIC_UPPERCASE string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const ALPHABETIC_LOWERCASE string = "abcdefghijklmnopqrstuvwxyz"
+const DIGITS string = "0123456789"
+const SYMBOLS string = "!\"#$%&'*+,-/:;<=>?@[\\]^_{}"
+
 type Charset map[rune]struct{}
 
 func NewCharset(characters string) Charset {
@@ -21,36 +26,5 @@ func (charset Charset) Add(characters string) {
 func (charset Charset) Remove(characters string) {
 	for _, c := range characters {
         delete(charset, c)
-    }
-}
-
-
-func GetAlphabeticUppercase() [26]rune {
-	return [26]rune {
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-        'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-        'U', 'V', 'W', 'X', 'Y', 'Z',
-    }
-}
-
-func GetAlphabeticLowercase() [26]rune {
-	return [26]rune {
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-        'u', 'v', 'w', 'x', 'y', 'z',
-    }
-}
-
-func GetDigits() [10]rune {
-	return [10]rune {
-		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-	}
-}
-
-func GetSymbols() [26]rune {
-	return [26]rune {
-        '!', '@', '#', '$', '%', '^', '&', '*', '_', '-',
-		'+', '=', '[', ']', '{', '}', '/', '\\', '?', '<',
-		'>', ',', ';', ':', '"', '\'',
     }
 }
