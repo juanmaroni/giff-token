998dfb758f4e93f816b6cf4617933e0d5ca61cfb
Juanma Romero <pro@juanmaroni.me>
2024-06-10 15:55:32 +0200

    Starting flags and help text

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"giff-token/generator"
 )
 
+const HELP_TEST = `
+༼ つ ◕_◕ ༽つ Giff Token (giff-token)
+`
+
 func main() {
-	fmt.Println(generator.GenerateToken());
+	flag.Usage = func() {
+		fmt.Print(HELP_TEST)
+	}
+
+	length := flag.Uint("length", 24, "Token length")
+	mode := flag.String("mode", "alphanumeric", "Mode to select base characters")
+	characters := flag.String("characters", "", "Use custom set of characters")
+	includeChars := flag.String("includeChars", "", "Custom charset to include")
+	excludeChars := flag.String("excludeChars", "", "Custom charset to exclude")
+
+	flag.Parse()
+
+	fmt.Println("length:", *length)
+    fmt.Println("mode:", *mode)
+	fmt.Println("characters:", *characters)
+    fmt.Println("includeChars:", *includeChars)
+	fmt.Println("excludeChars:", *excludeChars)
+    fmt.Println("tail:", flag.Args())
 }
