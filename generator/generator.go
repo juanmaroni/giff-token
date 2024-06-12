2d93803757010cc03929f50cba76b2f604f0faf0
Juanma Romero <pro@juanmaroni.me>
2024-06-12 16:48:44 +0200

    Function for generating a token

--- generator/generator.go
+++ generator/generator.go
@@ -1,6 +1,23 @@
 package generator
 
-func GenerateToken() string {
+import (
+	"giff-token/token"
+	"math/rand"
+	"time"
+)
 
-	return "thisisobviuslyfaketoken";
+func GenerateToken(config token.TokenConfig) string {
+	source := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(source)
+	
+	charset := config.Characters
+	charsetLen := len(charset)
+
+	token := make([]rune, config.Length)
+
+	for i := range token {
+		token[i] = charset[rng.Intn(charsetLen)]
+	}
+	
+	return string(token);
 }
